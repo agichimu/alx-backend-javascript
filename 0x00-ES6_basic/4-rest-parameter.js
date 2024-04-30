863c6d07abb482118a1209cb24383990ab8993d2
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:16:56 +0300

    Update 4-rest-parameter.js

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 export default function returnHowManyArguments(...args) {
   return args.length;
 }
-
-=======
-export default function returnHowManyArguments(...args) {
-  return args.length;
-}
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
