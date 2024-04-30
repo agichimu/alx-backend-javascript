ff0fc060037a22b4f7bf540fea4f687e6438d949
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:18:41 +0300

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-function getCurrentYear() {
-  const date = new Date();
-  return date.getFullYear();
-}
-
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return {
-    [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita,
-  };
-}
-
-=======
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
@@ -27,4 +12,3 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
 
   return budget;
 }
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
