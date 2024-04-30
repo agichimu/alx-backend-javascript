8720dcc53502d9632d97ecc0a64d849e3fa37218
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:16:09 +0300

    Update 2-arrow.js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
-  };
-}
-
-=======
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
@@ -18,4 +7,3 @@ export default function getNeighborhoodsList() {
     return self.sanFranciscoNeighborhoods;
   };
 }
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
