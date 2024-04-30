1bad59719d825c73f60ab37a0df337ea1c59eb7f
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:15:31 +0300

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
-
-  return [task, task2];
-}
-
-=======
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
@@ -23,4 +9,3 @@ export default function taskBlock(trueOrFalse) {
 
   return [task, task2];
 }
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
