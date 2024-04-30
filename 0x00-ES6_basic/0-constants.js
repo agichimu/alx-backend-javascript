1185662df88163d210f41149cf8386056244f269
agichimu <alexandergichimu98@gmail.com>
2024-04-30 10:13:41 +0000

    o

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
-}
-
-export function getLast() {
-  return ' is okay';
-}
-
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-  return combination;
-}
-
-=======
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -30,4 +13,3 @@ export function taskNext() {
 
   return combination;
 }
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
