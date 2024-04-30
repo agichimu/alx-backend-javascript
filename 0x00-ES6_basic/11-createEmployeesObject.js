ba3226b8d0b7ddc8b0c2bacc6ce22056c0f1037d
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:20:01 +0300

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-export default function createEmployeesObject(departmentName, employees) {
-  return { [departmentName]: employees };
-}
-
-=======
 export default function createEmployeesObject(departmentName, employees) {
   return {
     [departmentName]: [
@@ -11,4 +5,3 @@ export default function createEmployeesObject(departmentName, employees) {
       ],
     };
 }
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
