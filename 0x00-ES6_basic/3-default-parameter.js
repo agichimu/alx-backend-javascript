6bf5551f345bbc624c88149e1c2411654dbf3ee1
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:16:30 +0300

    Update 3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
-
-=======
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  return initialNumber + expansion1989 + expansion2019;
-}
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
