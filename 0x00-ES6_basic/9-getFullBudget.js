cd217381e41049fadce941911941161ccf128d99
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:19:08 +0300

    Update 9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import getBudgetObject from './7-getBudgetObject.js';
-
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
-  };
-
-  return fullBudget;
-}
-
-=======
 import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
@@ -33,4 +14,3 @@ export default function getFullBudgetObject(income, gdp, capita) {
 
   return fullBudget;
 }
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
