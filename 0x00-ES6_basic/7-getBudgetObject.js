95d0b0ba32c5dce4f92271ae6fce8e2a56df46a0
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:18:07 +0300

    Update 7-getBudgetObject.js

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-export default function getBudgetObject(income, gdp, capita) {
-  return { income, gdp, capita };
-}
-
-=======
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
     income,
@@ -13,4 +7,3 @@ export default function getBudgetObject(income, gdp, capita) {
 
   return budget;
 }
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
