9a864bc1e36d12b839a1eb6b45b10df350f3caf5
agichimu <alexandergichimu98@gmail.com>
2024-04-30 13:17:21 +0300

    Update 5-spread-operator.js

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 export default function concatArrays(array1, array2, string) {
   return [...array1, ...array2, ...string];
 }
-
-=======
-export default function concatArrays(array1, array2, string) {
-  return [...array1, ...array2, ...string];
-}
->>>>>>> 4d6f73f853c0741afdfb5a465c68c3cb940fad05
